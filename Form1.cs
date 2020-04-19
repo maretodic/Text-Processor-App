8167b45c61708784752f4c0bf27744f0b87a6989
Marko <todic.marko@outlook.com>
2020-04-19 16:00:05 +0200

    Added tab order and key press methods

--- Form1.cs
+++ Form1.cs
@@ -15,6 +15,44 @@ namespace TextProcessor
         public Form1()
         {
             InitializeComponent();
+            listViewDisplay.DoubleClick += new EventHandler(ListViewDisplay_DoubleClick);
+            listViewDisplay.KeyDown += ListViewDisplay_KeyDown;
+            searchButton.KeyDown += SearchButton_KeyDown;
+
+            extensionCombo.DropDownStyle = ComboBoxStyle.DropDownList;
+            extensionCombo.Items.Add("txt");
+            extensionCombo.Items.Add("csv");
+            extensionCombo.SelectedItem = "txt";
+        }
+
+        private void SearchButton_KeyDown(object sender, KeyEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ListViewDisplay_KeyDown(object sender, KeyEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ListViewDisplay_DoubleClick(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void folderSearchButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void viewButton_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
